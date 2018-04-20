70839603791ab4a714e65e20480ac36fb8a1df03
caezar47 <iss47@ya.ru>
2018-04-20 08:17:02 +0300

    ready

--- src/block/site/default/button/.document-ready.js
+++ src/block/site/default/button/.document-ready.js
@@ -7,3 +7,7 @@ $('.modal-search__modal').on('show.bs.modal', function (e) {
 $('.modal-search__modal').on('hidden.bs.modal', function (e) {
 	$('.navbar__btn-settings').toggleClass('is--open');
 })
+$('.card-item__close.is--tooltip').on('click', function (e) {
+	$(this).parent(".card-item__card.is--tooltip").toggleClass('is--open');
+})
+
